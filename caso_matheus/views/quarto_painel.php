510f85be5f11559ecd62815059154f8b69bbd0b5
MatheusFelix17 <42388283+MatheusFelix17@users.noreply.github.com>
2021-01-04 20:25:22 -0300

    Update quarto_painel.php
    Atualizando quarto_painel que agora contém a visualização do estado do dispositivo.

--- caso_matheus/views/quarto_painel.php
+++ caso_matheus/views/quarto_painel.php
@@ -6,46 +6,79 @@
 	<body>
 		<h3>Painel de Controle Quarto Matheus</h3>
 			
-		<table class="table table-hover table-striped" style="border:1px solid #ccc; width: 100%">
+		<table class="table table-hover table-striped" style="border:1px solid #ccc; width: 100%" id="quarto_painel">
 			<thead>
 				<tr>
-					<th style="width: 10px">#</th>
-					<th>Objeto</th>
-					<th>Acender</th>
-					<th style="width: 10px">Apagar</th>
+					<th>Dispositivo</th>
+					<th>Pino</th>
+					<th>Ligar</th>
+					<th>Desligar</th>
+					<th>Estado</th>					
 				</tr>
 			</thead>
+			
 			<tbody>
-
-				<tr>
-					<td>1-</td>
-					<td><b>Lâmpada</b> [GPIO 12]</td>
-					<td>
-						<form action="" method="post"> <!--/* get enviado url; post criptografado: action qual endereco que sera enviado; eh a nossa pagina pro ex o processa_curso.php */-->
-							<button type="submit" class="btn btn-block btn-success btn-lg" name="turnon" value="Acender">Acender</button>
-						</form>
-					</td>
-					<td>
-						<form action="" method="post">
-							<button type="submit" class="btn btn-block btn-success btn-lg" name="turnoff" value="Apagar">Apagar</button>
-						</form>		
-					</td>
-				</tr>	
-			</tbody>
-		</table>		
+				<! --Varrer o resultado da conexao*/ -->
+				<?php
+					while($linha = mysqli_fetch_array($consulta_dispositivos)) { 
+						echo '<tr><td>'.$linha['nome_dispositivo'].'</td>';
+						echo '<td>'.$linha['pino'].'</td>';
+					
+				?> 
+						<td><form action="" method="post">
+							<button type="submit" class="btn btn-block btn-success btn-lg" name="turnon" value=<?php echo $linha['pino']; ?>>Ligar</button>
+							</form>	
+						</td>
+						<td><form action="" method="post">
+							<button type="submit" class="btn btn-block btn-success btn-lg" name="turnoff" value=<?php echo $linha['pino']; ?>>Desligar</button>
+							</form>	
+						</td>
+						<td>
+							<?php
+								if($linha['estado'] == 1){
+									echo'<span style="color: Green;">
+										<i class="fas fa-circle"></i>
+									</span>';
+								}
+								else {
+									echo '<span style="color: Red;">
+										<i class="fas fa-circle"></i>
+									</span>';
+								}
+							?>
+						</td>
+						</tr>
+				<?php
+					
+					}
+				?>
+			</tbody>	
+		</table>
 	</body>
 </html>
 
 <?php 
 
 if (isset($_POST['turnon'])) {
- #$a = shell_exec("sudo python3 /var/www/html/gpio/gpio05/acender.py");
- #echo $a;
+	#$a = shell_exec("sudo python3 /var/www/html/gpio/gpio05/acender.py");
+	#echo $a;
 	echo "Acendeu!";
+	#echo  '<br />The ' . $_POST['turnon'] . ' ligou<br />';
+	#shell_exec("sudo python /var/www/painel.com/public_html/scripts/acender.py");
+	$num_pino = $_POST['turnon'];
+	$query = "UPDATE dispositivos SET estado = '1' WHERE dispositivos.pino = $num_pino";
+	mysqli_query($conexao, $query);
+	header('location:index.php?pagina=quarto_painel');
 }
  
 else if (isset($_POST['turnoff'])) {
- #$a = shell_exec("sudo python3 /var/www/html/gpio/gpio05/acender.py");
- #echo $a;
+	#$a = shell_exec("sudo python3 /var/www/html/gpio/gpio05/acender.py");
+	#echo $a;
 	echo "Apagou!";
+	#echo  '<br />O pino ' . $_POST['turnoff'] . ' foi desligado <br />';
+	#shell_exec();
+	$num_pino = $_POST['turnoff'];
+	$query = "UPDATE dispositivos SET estado = '0' WHERE dispositivos.pino = $num_pino";
+	mysqli_query($conexao, $query);
+	header('location:index.php?pagina=quarto_painel');
 } 
